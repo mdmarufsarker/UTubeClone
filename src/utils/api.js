3f5b77aa6ddfc9fb634a19a0bee7ad40cfe9e649
mdmarufsarker <mdmarufsarker2021@gmail.com>
2023-03-04 00:32:45 +0600

    youtube cloned

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,24 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = 'https://youtube138.p.rapidapi.com'
+const BASE_URL = "https://youtube138.p.rapidapi.com";
 
 const options = {
-//   method: 'GET',
-//   url: '/auto-complete/',
-  params: {hl: 'en', gl: 'US'},
-  headers: {
-    'X-RapidAPI-Key': import.meta.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-  }
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
 };
 
 export const fetchDataFromApi = async (url) => {
-    const response = await axios.get(`${BASE_URL}/${url}`, options);
-    return response.data;
-}
-
-// axios.request(options).then(function (response) {
-// 	console.log(response.data);
-// }).catch(function (error) {
-// 	console.error(error);
-// });
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
